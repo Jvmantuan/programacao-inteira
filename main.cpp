2585830c488bbc1723749e8cb5e4a1905c4ed130
Joao Victor Oliveira <jvoliveira3011@gmail.com>
2020-03-04 01:33:55 -0300

    método string_to_array não funcional

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <new>
 
 using namespace std;
 
@@ -12,12 +13,51 @@ using namespace std;
     **  5. Simplificar o problema
 */
 
+// Função string to array, converte uma string de numeros em um array desses numeros
+int *string_to_array(string str){
+    int k= 0, j = 0;
+    string c;
+    int *arr = new(nothrow) int[str.length()];
+
+    if(arr != nullptr) {
+        // for (int i = 0; i < str.length(); i++) {
+        //     j = i;
+        //     if (str[i] != ' '){ 
+        //             while(str[j] != ' ' ){
+        //                 c += str[j];
+        //                 j++;
+        //                 if(str[j] == ' '){
+        //                     continue;
+        //                 }
+        //             }
+        //             arr[i] = stoi(c);   
+        //     }
+        // }
+        for (int i = 0; i < str.length(); i++) {
+            j = i;
+            while (str[j] != ' '){
+                --j;
+                if(str[j] == ' '){
+                    ++j;
+                    while (str[j] != ' '){
+                        c += str[j];
+                        ++j;
+                    }
+                }
+            }
+            i = j;
+            arr[i] = stoi(c);
+        }
+        return arr;
+    }
+}
+
 // Printa a matriz desejada
 void printa_matriz(int **matriz, int m, int n){
     for (int i = 0; i < m; i++){
         for (int j = 0; j < n; j++)
             cout << matriz[i][j] << " ";
-        
+
         cout << endl;
     }
 }
@@ -34,16 +74,16 @@ int **aloca_matriz(int **mat,int m, int n){
         }
     }
 
-return mat;
+    return mat;
 }
 
 int main(){
 
-    ifstream entrada;
+    ifstream entrada; // var responsavel pela abertura de um arquivo
     string linha;
-    int m = 0, n = 0, contador = 0, **matriz = nullptr;
+    int m = 0, n = 0, contador = 0, **matriz = nullptr, *restr = nullptr;
 
-    entrada.open("entrada.txt");
+    entrada.open("entrada.txt"); // abre o arquivo
 
     if(!entrada.is_open()) {
         cerr << "Erro ao abrir o arquivo.";
@@ -55,20 +95,35 @@ int main(){
     // Usa um contador para armazenar o numero da linha atual do arquivo, e obter os dados do mesmo
     while(!entrada.eof()) {
         getline(entrada,linha);
-        cout << linha << endl;
+        //cout << linha << endl;
         if(contador == 0)
             m = stoi(linha);
-        else if(contador == 1)
+        else if(contador == 1){
             n = stoi(linha);
+            restr = new(nothrow) int[n-2];
+        }
+        else{
+            // Aqui printa a linha atual e embaixo printa os elementos dessa linha convertidos em int
+            // pelo método string_to_array()
+            cout << "Linha: " << linha << endl;
+            cout << "linha em um array de int: " << endl;
+            for (int i = 0; i < linha.length(); ++i) {
+                cout << string_to_array(linha)[i] << " ";
+            }
+            cout << "tamanho da linha: " << linha.length() <<endl;
+            //restr[contador-2] = *string_to_array(linha);
+        }
         contador++;
     }
-    entrada.close();
+    entrada.close(); //fecha o arquivo
 
     // Matriz de m linhas por n colunas
     matriz = aloca_matriz(matriz, m, n);
-    
-    cout << "\n" << "matriz " << m << "x" << n << endl;
-    printa_matriz(matriz, m, n);
+
+    //cout << "\n" << "matriz " << m << "x" << n << endl;
+    //printa_matriz(matriz, m, n);
+    //for (int i = 0; i < m-2; i++)
+    //cout << linha.length() << " ";
 
     delete[] matriz;
 }
---
+++ makefile.bat
@@ -0,0 +1,2 @@
+g++ -std=c++17 -pedantic -Wall main.cpp -o main
+main
