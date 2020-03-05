c7f419cc012a3dec390a0e83cbb99be6d4b0e4bd
Joao Victor Oliveira <jvoliveira3011@gmail.com>
2020-03-05 09:51:52 -0300

    DEU MERDA

--- pi.cpp
+++ pi.cpp
@@ -17,29 +17,71 @@ vector<string> split(const string &s, char delimiter){
     return result;
 }
 
+int **aloca_matriz(int **mat,int m, int n){
+    int i;
+    mat = (int **) calloc(m, sizeof(int *));
+    for(i = 0; i < m; i++){
+        mat[i] = (int *) calloc(n, sizeof(int));
+        if(mat[i] == nullptr){
+            cout << "Erro de alocacao.";
+            return nullptr;
+        }
+    }
+
+    return mat;
+}
+
+void printa_matriz(int **matriz, int m, int n){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++)
+            cout << matriz[i][j] << " ";
+
+        cout << endl;
+    }
+}
+
 int main(){
     ifstream entrada;
     string token, linha, s, delimiter = " ";
     size_t pos = 0;
-    vector<string> v, int_v;
+    vector<string> v;
+    vector<string> int_v;
+    int cont = 0, m, n, **matriz = nullptr;
 
     entrada.open("entrada.txt");
-
+	
+	
+	
     while(!entrada.is_open()) {
         cerr << "Erro";
         return false;
     }
-
+	
     while(!entrada.eof()) {
         getline(entrada,linha);
-        v = split(linha, ' ');
+        if(cont == 0)
+        	m = stoi(linha);
+        else if(cont == 1)
+        	n = stoi(linha);
+        cont++;
+	}
+	entrada.close();
+	
+	entrada.open("entrada.txt");
+	
+	matriz = aloca_matriz(matriz, m,n);
+	
+	while(!entrada.eof()) {
+		getline(entrada,linha);
+		v = split(linha, ' ');
         for(int i = 0; i < v.size(); i++){
-            cout << v[i] << " ";
-            int_v[i] = stoi(v[i]);
-        }
-
-        cout << endl;
-    }
-    cout << int_v[0];
+        	
+			//	cout << v[i] << " ";
+            //int_v[i] = v[i];
+       	}
+	}
+	entrada.close();
+	printa_matriz(matriz,m,n);
+    //cout << int_v[0];
     return 0;
 }
